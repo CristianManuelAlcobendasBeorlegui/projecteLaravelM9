3a98b1b695ca7947b96afc59188f41adb92ed5f6
Yassine <yassineelbakali30@gmail.com>
2024-01-22 19:16:55 +0100

    Scaffolding de la taula Destinacions

---
+++ app/Http/Controllers/DestinacionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreDestinacionRequest;
+use App\Http\Requests\UpdateDestinacionRequest;
+use App\Models\Destinacion;
+
+class DestinacionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDestinacionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Destinacion $destinacion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Destinacion $destinacion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDestinacionRequest $request, Destinacion $destinacion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Destinacion $destinacion)
+    {
+        //
+    }
+}
---
+++ database/seeders/DestinacionSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DestinacionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
