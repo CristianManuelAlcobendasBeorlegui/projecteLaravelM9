e25d9dcc904f221273d5170d3669d1aec56c29da
Cristian Manuel Alcobendas Beorlegui <cristian.alcobendas@insbaixcamp.cat>
2024-01-22 23:11:47 +0100

    Modificació i estils per la vista INDEX de Destinacions

--- resources/views/destinacions/index.blade.php
+++ resources/views/destinacions/index.blade.php
@@ -4,22 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        .container {
-            border: 2px solid black;
-            padding: 2em 1.5em;
-            margin-bottom: 1em;
-        }
-    </style>
+    <!-- Stylesheet file -->
+    <link rel="stylesheet" href="{{ asset('./css/generalStyles.css') }}">
 </head>
 <body>
-    @foreach ($destinacions as $destinacion)
-    <div class="container">
-        <img src="{{ asset($destinacion->picture_location) }}" alt="">
-        <h1>{{ $destinacion->name }}</h1>
-        <h2>{{ $destinacion->province }} - {{ $destinacion->country }}</h2>
-        <a href="{{route('destinacions.show', $destinacion->id)}}">Ver mas</a>
-    </div>
-    @endforeach
+    <header>
+        <div id="logo">
+            <h1>Logo</h1>
+        </div>
+        <div id="opcionesUsuario">
+            <a href="{{ route('register') }}">Registrarse</a>
+            <a href="{{ route('login') }}">Iniciar sesión</a>
+        </div>
+    </header>
+    <main>
+        <section id="planificaTusVacaciones">
+            <h2>¡Planifica tus vacaciones!</h2>
+            <p>¿Quieres desconectar de la rutina? ¡Hecha un vistazo a los destinos que te proponemos!</p>
+            <div id="eligeUnDestino">
+                <h3>Destinos</h3>
+                <div id="destinos">
+                    
+                    @foreach ($destinacions as $destinacion)
+                    <div class="destino">
+                        <div class="imagenDestino">
+                            <img src="{{ asset($destinacion->picture_location) }}" alt="Imagen destino {{ $destinacion->name }}">
+                        </div>
+                        <div class="datosDestino">
+                            <h4>{{ $destinacion->name }}</h4>
+                            <h5>{{ $destinacion->province }} - {{ $destinacion->country }}</h5>
+                            <div class="opcionesDestino">
+                                <a href="{{ route('destinacions.show', $destinacion->id) }}">Ver detalles</a>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer>
+        <div id="copyrightInfo">
+            &copy; 2024. M.09 - UF2 - Proyecto
+        </div>
+    </footer>
 </body>
 </html>
