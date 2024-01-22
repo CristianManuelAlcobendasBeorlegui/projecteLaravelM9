e14a8f4892b97f0df7e7d6dd1f2464627f1c3a65
Cristian Manuel Alcobendas Beorlegui <cristian.alcobendas@insbaixcamp.cat>
2024-01-22 23:12:55 +0100

    Modificacions i estils per la vista SHOW de Destinacions

--- resources/views/destinacions/show.blade.php
+++ resources/views/destinacions/show.blade.php
@@ -1,24 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-
+    <!-- Stylesheet file -->
+    <link rel="stylesheet" href="{{ asset('./css/generalStyles.css') }}">
 </head>
+
 <body>
-    <div class="container">
-        <div id="image">
-            <img src="{{ asset($destinacions->picture_location) }}" alt="">
+    <header>
+        <div id="logo">
+            <h1>Logo</h1>
         </div>
-        <div id="placeInfo">
-            <h1>{{ $destinacions->name }}</h1>
-            <h2>{{ $destinacions->province }} - {{ $destinacions->country }}</h2>
-            <p>
-                {{ $destinacions->description }}
-            </p>
+        <div id="opcionesUsuario">
+            <a href="{{ route('register') }}">Registrarse</a>
+            <a href="{{ route('login') }}">Iniciar sesión</a>
         </div>
-        <a href="{{ route('destinacions.index') }}">Volver</a>
-    </div>
+    </header>
+    <main>
+        <section id="detallesLugar">
+            <div class="principal">
+                <div id="imagenLugar">
+                    <img src="{{ asset($destinacions->picture_location) }}" alt="Imagen del destino {{ $destinacions->name }}">
+                </div>
+                <div id="descripcion">
+                    <h2>{{ $destinacions->name }}</h2>
+                    <h3>{{ $destinacions->province }} - {{ $destinacions->country }}</h3>
+                    <p>{{ $destinacions->description }}</p>
+                </div>
+            </div>
+            <div class="secundario">
+                <div class="opciones">
+                    <a href="{{ route('destinacions.index') }}">¡Ver más destinos!</a>
+                </div>    
+            </div>
+        </section>
+    </main>
+    <footer>
+        <div id="copyrightInfo">
+            <span>&copy; 2024. M.09 - UF2 - Proyecto</span>
+        </div>
+    </footer>
 </body>
+
 </html>
